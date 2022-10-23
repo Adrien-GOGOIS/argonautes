b74d29da38b60775dabd818e12cfe3bd80ad8603
Adrien GOGOIS <ad.gogois@gmail.com>
2022-10-23 15:30:58 +0200

    Improve style with Tailwind

--- src/App.js
+++ src/App.js
@@ -45,17 +45,18 @@ function App() {
 
       <main>
         
-        <h2>Ajouter un(e) Argonaute</h2>
+        <h2 className='pt-2 text-wildcodeschool'>Ajouter un(e) Argonaute</h2>
         <form className="new-member-form" onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="name">Nom de l&apos;Argonaute</label>
           <input 
+            className='p-2 border-2 border-wildcodeschool mt-2'
             id="name" 
             name="name" 
             type="text" 
             placeholder="Charalampos" 
             {...register("name", { required: true, maxLength: 50, minLength: 1,})}
           />
-          <button type="submit">Envoyer</button>
+          <button type="submit" className='border-2 rounded p-2 ml-2 bg-wildcodeschool hover:bg-white border-wildcodeschool'>Envoyer</button>
           {errors.name && (
                 <span className="">
                   Merci d'indiquer un nom de membre d'équipage entre 1 et 50 caractères
@@ -63,23 +64,23 @@ function App() {
               )}
         </form>
         
-        <h2>Membres de l'équipage</h2>
-        <section className="member-list">
+        <h2 className='mb-6 text-wildcodeschool text-2xl'>Membres de l'équipage</h2>
+        <section className="member-list grid grid-cols-3 gap-4">
           {members && members.map((member) => {
             return (
-              <>
-              <div key={member.name} className="member-item">{member.name}</div>
-                <button className="" onClick={() => deleteMember(member.member_id)}>
+              <div className='flex flex-row my-2'>
+                <button className="border-2 border-red-600 rounded mr-2 px-2 bg-red-600 text-white hover:bg-white hover:text-black" onClick={() => deleteMember(member.member_id)}>
                     X
                 </button>
-              </>  
+                <div key={member.name} className="member-item uppercase">{member.name}</div>
+              </div>  
             )
           })}
         </section>
       </main>
 
-      <footer>
-        <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
+      <footer className='w-full absolute bottom-0'>
+        <p>Réalisé par Adrien en Anthestérion de l'an 515 avant JC</p>
       </footer>
       </>
   );
